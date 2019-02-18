f557124ac3f7ed6d6cf6302c13f08cb1b0b11e6f
Naimar1 <naiglyme1@gmail.com>
2019-02-18 12:37:40 +0200

    Put if condition in my index file and import route in my views file

--- app/views.py
+++ app/views.py
@@ -9,3 +9,13 @@ def news(news_article):
     View news page function that returns the news details page and its articles
     '''
     return render_template('news.html',title = news_article)
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    heading = 'Home - Welcome tto our News Highlight'
+    return render_template('index.html', heading = heading)
